4970a2b9a01ee388f81d8155000c0bc9ac4ab90d
apkalisha <39853483+apkalisha@users.noreply.github.com>
2018-06-20 20:42:16 +0530

    Initial commit

---
+++ app/src/main/java/com/indian/states/capitals/indianstates/DetailActivity.java
@@ -0,0 +1,206 @@
+package com.indian.states.capitals.indianstates;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ms.square.android.expandabletextview.ExpandableTextView;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class DetailActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private ProgressBar mProgressBar;
+    private CoordinatorLayout coordinatorLayout;
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+    YouTubePlayerView youTubePlayerView;
+
+    CircleIndicator circleIndicator;
+    private AutoScrollViewPager viewPager;
+    private PagerAdapter adapter;
+
+    private ArrayList<String> images;
+    private ArrayList<String> imageDetails;
+
+    private DatabaseReference databaseReference;
+
+    private TextView capital, area, population,literacyRate,literacyRateMale,literacyRateFemale,sexRatio;
+    private ImageView appbarImageView;
+    private ExpandableTextView historyTextView, languages, regionalDance;
+
+    private String youTubeVideoLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+
+
+        Toolbar toolbar=findViewById(R.id.detail_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+
+
+        viewPager = findViewById(R.id.view_pager);
+        circleIndicator = findViewById(R.id.indicator);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        viewPager.startAutoScroll();
+        viewPager.setInterval(5000);
+        viewPager.setCycle(true);
+        viewPager.setStopScrollWhenTouch(true);
+
+        //Allow scrolling through ViewPager
+
+        viewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                viewPager.getParent().requestDisallowInterceptTouchEvent(true);
+            }
+        });
+
+        youTubePlayerView =  findViewById(R.id.youtube_player_view);
+        youTubePlayerView.initialize(BuildConfig.ApiKey, this);
+
+        population = findViewById(R.id.population);
+        capital = findViewById(R.id.capital);
+        area = findViewById(R.id.area);
+
+        coordinatorLayout = findViewById(R.id.detail_container);
+        appbarImageView = findViewById(R.id.app_bar_image);
+        historyTextView = findViewById(R.id.expand_text_view);
+        languages=findViewById(R.id.languages_spoken);
+        regionalDance=findViewById(R.id.regional_dance);
+        literacyRate = findViewById(R.id.literacy_rate);
+        literacyRateMale = findViewById(R.id.literacy_rate_male);
+        literacyRateFemale = findViewById(R.id.literacy_rate_female);
+        sexRatio = findViewById(R.id.sex_ratio);
+
+        Intent intent = getIntent();
+        if(intent.hasExtra("State")) {
+            collapsingToolbarLayout.setTitle(intent.getStringExtra("State"));
+            databaseReference = FirebaseDatabase.getInstance().getReference().child("States").child(intent.getStringExtra("State").trim());
+        }
+
+
+
+
+        //Connectivity Manager
+        ConnectivityManager connectivityManager = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected() || !networkInfo.isAvailable()){
+            Snackbar snackbar = Snackbar.make(findViewById(R.id.detail_activity),"No Internet Connection", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+
+
+    }
+
+    private void readDataFromDatabase(final StatesCallback statesCallback) {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                StateDetails stateDetails = dataSnapshot.getValue(StateDetails.class);
+                statesCallback.onCallback(stateDetails);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, final YouTubePlayer youTubePlayer, final boolean b) {
+
+        readDataFromDatabase(new StatesCallback() {
+            @Override
+            public void onCallback(StateDetails stateDetails) {
+                capital.setText(stateDetails.getCapital());
+                area.setText(stateDetails.getArea());
+                population.setText(stateDetails.getPopulation());
+                historyTextView.setText(stateDetails.getHistory());
+                languages.setText(stateDetails.getLanguages());
+                regionalDance.setText(stateDetails.getRegionalDance());
+                literacyRate.setText(String.valueOf(stateDetails.getLiteracyRate()));
+                literacyRateMale.setText(String.valueOf(stateDetails.getLiteracyRateMale()));
+                literacyRateFemale.setText(String.valueOf(stateDetails.getLiteracyRateFemale()));
+                sexRatio.setText(String.valueOf(stateDetails.getSexRatio()));
+                images = stateDetails.getImages();
+                imageDetails = stateDetails.getImageDetails();
+                youTubeVideoLink = stateDetails.getYouTubeVideoLink();
+
+                Picasso.get().load(images.get(0)).into(appbarImageView);
+                adapter = new CustomAdapter(DetailActivity.this, images, imageDetails);
+                viewPager.setAdapter(adapter);
+                circleIndicator.setViewPager(viewPager);
+                adapter.registerDataSetObserver(circleIndicator.getDataSetObserver());
+                if(!b) {
+                    Log.i("video link",youTubeVideoLink);
+                    youTubePlayer.cueVideo(youTubeVideoLink);
+                }
+                mProgressBar.setVisibility(View.GONE);
+                coordinatorLayout.setVisibility(View.VISIBLE);
+
+
+
+            }
+        });
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if(youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(DetailActivity.this,RECOVERY_DIALOG_REQUEST);
+        } else {
+            String errorMessage = getString(R.string.youTubeErrorMessage)+youTubeInitializationResult;
+            Toast.makeText(DetailActivity.this,errorMessage,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/indian/states/capitals/indianstates/MainActivity.java
@@ -0,0 +1,155 @@
+package com.indian.states.capitals.indianstates;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean doubleBackToExitPressedOnce = false;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
+
+                //  Create a new boolean and preference and set it to true
+                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+                //  If the activity has never started before...
+                if (isFirstStart) {
+
+                    //  Launch app intro
+                    final Intent i = new Intent(MainActivity.this, AppIntro.class);
+
+                    runOnUiThread(new Runnable() {
+                        @Override public void run() {
+                            startActivity(i);
+                        }
+                    });
+
+                    //  Make a new preferences editor
+                    SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    e.apply();
+                }
+            }
+        });
+
+        // Start the thread
+        t.start();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        selectedFragment = HomeFragment.newInstance();
+                        break;
+                    case R.id.navigation_bookmarks:
+                        selectedFragment = BookmarkFragment.newInstance();
+                        break;
+                    case R.id.navigation_profile:
+                        selectedFragment = ProfileFragment.newInstance();
+                        break;
+                    case R.id.navigation_settings:
+                        selectedFragment = SettingsFragment.newInstance();
+                        break;
+
+                }
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame_layout,selectedFragment);
+                fragmentTransaction.commit();
+                return true;
+            }
+        });
+
+
+
+        //reselected item will not redirect to same fragment
+
+        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+            }
+        });
+
+
+
+        setDefaultFragment();
+
+
+
+        //Connectivity Manager
+        ConnectivityManager connectivityManager = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo == null || !networkInfo.isConnected() || !networkInfo.isAvailable()){
+            Snackbar snackbar =Snackbar.make(findViewById(R.id.main_activity), "No Internet Connection", Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+
+    }
+
+    private void setDefaultFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,HomeFragment.newInstance());
+        fragmentTransaction.commit();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Snackbar.make(findViewById(R.id.snackbar_container),"Press back again to exit",Snackbar.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+}
